e18069e621fc667cd1d1174385bffd37ebb9ef27
Tarek Khachnaoui <t.khachnaoui@oodrive.com>
2021-05-07 09:50:03 +0100

    first commit

---
+++ clone_docker_compose.sh
@@ -0,0 +1,6 @@
+rm /home/vagrant/workspace
+mkdir /home/vagrant/workspace
+cd /home/vagrant/workspace
+git clone https://github.com/Tarekdh/odoo-docker-compose.git
+cd odoo-docker-compose
+docker-compose up
